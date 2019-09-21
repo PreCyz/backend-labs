d7cfce23f3082dcd3fe80419fa0f9b8caaf48d67
PreCyz <precpaw@op.pl>
2019-09-21 23:37:13 +0200

    Making it work with AWS.

--- src/test/java/pw/backend/lab/backlab/BackLabApplicationTests.java
+++ src/test/java/pw/backend/lab/backlab/BackLabApplicationTests.java
@@ -3,10 +3,12 @@ package pw.backend.lab.backlab;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles(profiles = {"dev"})
 public class BackLabApplicationTests {
 
 	@Test
